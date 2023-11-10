faac559718b5afbb177da08500e31107fb1b5dd2
Állyson Philipe <88353432+allysonlipe@users.noreply.github.com>
2023-11-10 19:27:55 -0300

    Frist Commit

---
+++ instalacoes.js
@@ -0,0 +1,11 @@
+/* 
+Programas:
+nodeJS: Roda o código no pc (Não é uma extensão.)
+
+Extensões VScode
+Code Runner: Permite rodar o código no console ; Configurar: rodar no terminal, salvar antes de rodar, limpar console antes de rodar.
+Dracula Oficial: Deixa o VSCode mais amigável
+JavaScript (ES6) code snippets: Ajuda com emmet
+Material Icon Theme: Deixa os ícones mais bonitos.
+
+ */
